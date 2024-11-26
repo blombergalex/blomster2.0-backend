ad88a2aa3d5ea18b72aa24c6e367f06d1e60cb6f
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-26 12:00:48 +0100

    setup backend

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { postRouter } from './routes/post'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(postRouter)
+
+mongoose.connect(process.env.DB_URL!).then(() => {
+  const port = process.env.PORT || '8080'
+  app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}/`)
+  }) 
+})
+
+
+
