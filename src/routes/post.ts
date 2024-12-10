f351eddfe8e6012244c632c03179cba4b15eba5e
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-12-10 09:13:57 +0100

    create create post action

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from "express"
+import { Post } from "../models/post"
+import { authenticate } from "../middlewares/authenticate"
 
 const getPosts = async (req: Request, res: Response) => {
   // TODO: get posts from database
@@ -15,13 +17,37 @@ const getPost = async (req: Request, res: Response) => {
 
 const createPost = async (req: Request, res: Response) => {
   // TODO: post post to database
+  try {
+    const {title, content} = req.body
 
-  res.status(200).json([{title: 'create post'}])
+    if (!title || typeof title !== 'string') {
+      res.status(400).json({message: 'Malformed title'})
+    }
+
+    if (content !== undefined && typeof content !== 'string') {
+      res.status(400).json({message: 'Malformed content'})
+    }
+
+    const post = await Post.create({
+      title,
+      content,
+      author: req.userId
+    })
+
+    res.status(201).json({ id: post._id })
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send()
+  }
+
 }
 
 export const postRouter = Router()
 
 postRouter.get('/posts', getPosts) // show all posts
+postRouter.get('/posts/:id', getPost) // get post by id
+postRouter.post('/posts', authenticate, createPost)
+
 postRouter.get('/create', createPost) // render create post page
 postRouter.get('/posts/:id/edit', getPost) // get post by id for edit page, put method from frontend?
-postRouter.get('/posts/:id', getPost) // get post by id
