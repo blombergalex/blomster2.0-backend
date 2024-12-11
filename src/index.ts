d01fcfa358d8a1c30a595f8200754b1b67858a49
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-12-11 12:43:33 +0100

    import cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+import cors from 'cors'
+import 'dotenv/config'
 import express from 'express'
+import mongoose from 'mongoose'
+
 import { postRouter } from './routes/post'
 import { authRouter } from './routes/auth'
 import { profileRouter } from './routes/profile'
-import mongoose from 'mongoose'
-import 'dotenv/config'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use(postRouter)
 app.use(authRouter)
